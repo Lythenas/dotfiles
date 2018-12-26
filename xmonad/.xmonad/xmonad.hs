9388686b25e90cc889002f8756558df48e8e5a44
Matthias Seiffert <lythenas@hotmail.de>
2018-12-26 16:17:04 +0100

    Extended xmonad and xmobar config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,10 +1,13 @@
+module Main (main) where
+
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Prompt.AppLauncher
+import XMonad.Prompt.XMonad
 import XMonad.Layout.NoBorders
 import Graphics.X11.ExtraTypes.XF86
 import System.IO
@@ -12,7 +15,7 @@ import Data.Maybe (maybeToList)
 import Control.Monad (when, join)
 
 main = do
-  xmproc <- spawnPipe "xmobar"
+  xmproc <- spawnPipe "~/.cabal/bin/xmobar"
   xmonad $ ewmh $ docks defaultConfig
       { terminal    = "alacritty"
       , modMask     = superMask
@@ -32,7 +35,11 @@ superMask = mod4Mask
 myManageHook = composeAll
     [ className =? "Gimp" --> doFloat ]
 
-myLayout = avoidStruts $ smartBorders $ tiled ||| Mirror tiled ||| Full
+-- Layout:
+--   -- Tall (with statusbar and borders)
+--   -- Mirror Tall (with statusbar and borders)
+--   -- Full (no statusbar and no borders)
+myLayout = smartBorders $ avoidStruts (tiled ||| Mirror tiled) ||| noBorders Full
     where
         tiled = Tall nmaster delta ratio
         nmaster = 1
@@ -66,11 +73,13 @@ myAdditionalKeys =
     , ((superMask, xK_p), spawn "dmenu-launch")
     -- changing the reload shortcut because xmessage is not working corretly on my system
     , ((superMask, xK_q), spawn "if type xmonad; then (xmonad --recompile || notify-send \"Error in xmonad.hs\") && xmonad --restart; else notify-send xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+    , ((superMask .|. shiftMask, xK_q), return ())
 
     -- Media Keys
     , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")
     , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+")
     , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
+    , ((0, xF86XK_AudioMicMute), spawn "amixer set Capture toggle")
     , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +5")
     , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -5")
     , ((0, xF86XK_Display), spawn "~/bin/displayselect")
@@ -79,6 +88,8 @@ myAdditionalKeys =
     , ((0, xF86XK_Search), spawn "notify-send Search")
     , ((0, xF86XK_LaunchA), spawn "notify-send LaunchA")
     , ((0, xF86XK_Explorer), spawn "notify-send Explorer")
+
+    , ((superMask, xK_F12), xmonadPrompt def)
     ]
 
 lockCommand = "xscreensaver-command -lock"
