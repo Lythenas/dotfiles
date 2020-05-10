d33c37e72353573ddcd77be584acfa1019deb167
Matthias Seiffert <matthias-seiffert@hotmail.de>
2020-05-10 14:59:14 +0200

    Update bspwm and sxhkd config

--- bspwm/.config/bspwm/external_rules.sh
+++ bspwm/.config/bspwm/external_rules.sh
@@ -5,7 +5,18 @@ class=$2
 instance=$3
 consequences=$4
 
-if xdotool getwindowname $wid | grep "Steam Guard" 2>&1 >/dev/null; then
+windowname=$(xdotool getwindowname $wid)
+
+echo -e "[$(date --rfc-3339=ns)]\nwid: $wid\nclass: $class\ninstance: $instance\nconsequences: $consequences\nwindowname: $windowname" >> ~/window.events
+
+eval $consequences
+
+if [[ "$class" == "Steam" ]] && echo "$windowname" | grep "Steam Guard" 2>&1 >/dev/null; then
+    # steam guard => floating
   echo state=floating
+elif [[ "$class" == "firefox" ]] && echo "$windowname" | grep "Opening" 2>&1 >/dev/null; then
+    # firefox save dialog => on current desktop
+    # echo desktop=focused
+    echo node=focused
 fi
 
---
+++ bspwm/.config/bspwm/setup_monitors.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# for desktop
+xrandr --output DP-2 --auto --primary \
+  --output DP-3 --auto --left-of DP-2
+
